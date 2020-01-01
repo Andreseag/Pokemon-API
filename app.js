5ad514e7e1622aeb16ad2d7df023c2430c5d984a
Andreseag94 <edwin.castro@quick.com.co>
2019-12-31 20:52:38 -0500

    Styles Cards Pokemons

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ const fetchPokemon = () =>{
 const displayPokemon = (pokemon) =>{
   console.log(pokemon);
   const pokemonsHTMLStrign = pokemon.map( pokeman => `
-    <p>
-      <img src="${pokeman.image}" />
-      <h2>${pokeman.id}. ${pokeman.name}</h2>
-      <p>Type: ${pokeman.type}</p>
-    </p>
+    <li class="card">
+      <img class="card-image" src="${pokeman.image}" />
+      <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+      <p class="card-subtitle">Type: ${pokeman.type}</p>
+    </li>
   ` ).join('');
   pokedex.innerHTML = pokemonsHTMLStrign;
 }
